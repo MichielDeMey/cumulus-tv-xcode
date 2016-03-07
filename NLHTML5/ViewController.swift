6dcd087255d73ef85b723ed730017f2e9d8fe097
Michiel De Mey <de.mey.michiel@gmail.com>
2016-03-07 18:54:44 +0100

    Initial Commit

---
+++ NLHTML5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NLHTML5
+//
+//  Created by Michiel De Mey on 07/03/16.
+//  Copyright © 2016 Michiel De Mey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
